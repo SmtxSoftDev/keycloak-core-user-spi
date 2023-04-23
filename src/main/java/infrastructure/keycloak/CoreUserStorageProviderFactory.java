fa27a1ab511f58ab8e42ea134b9779ddf7f98cc0
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-04-24 01:10:29 +0600

    Added list of database driver in user interface

--- src/main/java/infrastructure/keycloak/CoreUserStorageProviderFactory.java
+++ src/main/java/infrastructure/keycloak/CoreUserStorageProviderFactory.java
@@ -11,6 +11,7 @@ import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.provider.ProviderConfigurationBuilder;
 import org.keycloak.storage.UserStorageProviderFactory;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CoreUserStorageProviderFactory implements UserStorageProviderFactory<CoreUserStorageProvider> {
@@ -28,7 +29,9 @@ public class CoreUserStorageProviderFactory implements UserStorageProviderFactor
 
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
+        List<String> driverListOfDb = Arrays.asList("MSSQL", "MySql", "Postgresql");
         return ProviderConfigurationBuilder.create()
+                .property("Database driver", "Database driver", "Select database driver", ProviderConfigProperty.LIST_TYPE, "", driverListOfDb)
                 .property(Constants.DB_SERVER, "Database server", "Address of Sql server", ProviderConfigProperty.STRING_TYPE, "", null)
                 .property(Constants.DB_NAME, "Database name", "Database name", ProviderConfigProperty.STRING_TYPE, "", null)
                 .property(Constants.DB_USERNAME, "Database Username", "Database username", ProviderConfigProperty.STRING_TYPE, "", null)
