21c4450e378c6b5823fa0960b1e2a791c7eeebc2
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-05-19 17:14:16 +0600

    Refactoring

--- src/main/java/infrastructure/drivers/db/PostgresqlDriver.java
+++ src/main/java/infrastructure/drivers/db/PostgresqlDriver.java
@@ -10,7 +10,7 @@ public class PostgresqlDriver implements IDatabaseDriver{
     public Properties getProperties(ComponentModel model) {
         Properties properties = new Properties();
         properties.setProperty("hibernate.connection.url",
-                "jdbc:postgresql//"+model.get(Constants.DB_SERVER) +"/"+model.get(Constants.DB_NAME));
+                "jdbc:postgresql://"+model.get(Constants.DB_SERVER) +"/"+model.get(Constants.DB_NAME));
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         properties.setProperty("hibernate.connection.username", model.get(Constants.DB_USERNAME));
         properties.setProperty("hibernate.connection.password", model.get(Constants.DB_PASSWORD));
