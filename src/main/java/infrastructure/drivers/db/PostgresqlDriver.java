d0513093d2c403e40d74584dc0a521cc1c56542c
Абсатар уулу Мыктыбек <smtx.muktubek@gmail.com>
2023-05-19 17:12:09 +0600

    Added configuration property for postgresql class

--- src/main/java/infrastructure/drivers/db/PostgresqlDriver.java
+++ src/main/java/infrastructure/drivers/db/PostgresqlDriver.java
@@ -1,5 +1,6 @@
 package infrastructure.drivers.db;
 
+import domain.helpers.Constants;
 import org.keycloak.component.ComponentModel;
 
 import java.util.Properties;
@@ -7,6 +8,14 @@ import java.util.Properties;
 public class PostgresqlDriver implements IDatabaseDriver{
     @Override
     public Properties getProperties(ComponentModel model) {
-        return null;
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.connection.url",
+                "jdbc:postgresql//"+model.get(Constants.DB_SERVER) +"/"+model.get(Constants.DB_NAME));
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.connection.username", model.get(Constants.DB_USERNAME));
+        properties.setProperty("hibernate.connection.password", model.get(Constants.DB_PASSWORD));
+        properties.setProperty("hibernate.connection.driver_class", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("show_sql", "true");
+        return properties;
     }
 }
